e3aa5e5e5a8ddbce74613e8e2c50dd04abc51698
Vladimir <Luxorchegone@gmail.com>
2021-06-02 11:49:49 +0300

    Добавил небольшой список книг в сервис

--- src/services/book-store-service.js
+++ src/services/book-store-service.js
@@ -1,5 +1,21 @@
 export default class BookStoreService {
-    getBook() {
-        return [];
+    getBooks() {
+        return [
+            {
+                id: 1,
+                title: 'Production-ready Microservices',
+                author: 'Suzan J. Fowler'
+            },
+            {
+                id: 2,
+                title: 'Release It!',
+                author: 'Michael T. Nygard'
+            },
+            {
+                id: 3,
+                title: 'Grokking algorithms',
+                author: 'Adilya Y. Bhargava'
+            }
+        ];
     }
 }
