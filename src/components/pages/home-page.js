9ce4047058fa5fc4dbd5d9c634f0bc811bc46f0f
Vladimir <Luxorchegone@gmail.com>
2021-06-03 20:51:21 +0300

    Добавил небольшие коментарии

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -2,13 +2,12 @@ import React from 'react';
 import BookList from '../book-list/';
 import ShoppingCartTable from '../shopping-cart-table';
 
-const HomePage = () => {
+const HomePage = () => { //Домашняя страница приложения
     return (
         <div>
             <BookList />
             <ShoppingCartTable />
         </div>
-        
     );
 }
 export default HomePage;
