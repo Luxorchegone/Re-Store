829336b0fa74f86f03d2b6c8a9a0e2b6ffcc6b11
Vladimir <Luxorchegone@gmail.com>
2021-06-03 19:36:39 +0300

    Добваил стиль в разметку

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,19 +1,47 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import BookListItem from '../book-list-item/';
+import {connect} from 'react-redux';
+import {withBookStoreService} from '../hoc';
 import './book-list.css';
+import {booksLoaded} from '../../actions';
+import compose from '../../utils';
 
-const BookList = ({books}) => {
-        
-    return (
-        <ul>
-            {
-                books.map((book) => {
-                    return (
-                        <li key={book.id}><BookListItem book={book}/></li>
-                    )    
-                })
-            }
-        </ul>
-    );
-};
-export default BookList;
+class BookList extends Component {
+   
+    componentDidMount() {
+        const {bookStoreService} = this.props;
+        const data = bookStoreService.getBooks();
+        this.props.booksLoaded(data);
+    }
+    
+    
+    render () {
+        const {books} = this.props
+        return (
+            <ul className="book-list">
+                {
+                    books.map((book) => {
+                        return (
+                            <li key={book.id}><BookListItem book={book}/></li>
+                        )    
+                    })
+                }
+            </ul>
+        );
+    }
+}
+
+const mapStateToProps = ({books}) => {
+    return {
+        books: books
+    }
+}
+
+const mapDispatchToProps = {
+    booksLoaded
+}
+
+export default compose(
+    withBookStoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
