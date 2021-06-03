7f5187c4d5fc84e2aaad48b88f59e80ab6806cc4
Vladimir <Luxorchegone@gmail.com>
2021-06-03 17:26:52 +0300

    Создал компонент - шапку, пока что заглушка

---
+++ src/components/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './header.js';
+
+const Header = () => {
+    return (
+        <Link></Link>
+    );
+}
+export default Hedaer;
