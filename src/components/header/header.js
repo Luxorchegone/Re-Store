8217ef781dc2cb830d5310cb63d0b9a229d848ce
Vladimir <Luxorchegone@gmail.com>
2021-06-03 19:38:12 +0300

    Написал шапку

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import './header.js';
+import './header.css';
 
-const Header = () => {
+const Header = ({numItems, total}) => {
     return (
-        <Link></Link>
+        <header className="page-header">
+            <a className="page-header__logo text-dark" href="#">
+                Re-Store
+            </a>
+            <a className="shopping-cart text-dark" href="#">
+                <i className="shopping-cart__icon fa fa-shopping-cart"/>
+                {numItems} items (${total})
+            </a>
+
+        </header>
+      
     );
 }
-export default Hedaer;
+export default Header;
