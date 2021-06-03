ba800efb0b1287d1907884a686c1b2a1fab6533a
Vladimir <Luxorchegone@gmail.com>
2021-06-03 20:41:20 +0300

    переписал спиннер и добавил ему надпись loading...

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './spinner.css';
 
-const Spinner = () => {
+const Spinner = () => { //Индикатор загрузки
     return (
-        <div className="lds-hourglass">
+        <div className="spinner-wrapper">
+            <div className="lds-hourglass">
+            </div>
+            <h3>loading...</h3>
         </div>
     );
 }
