4a3885acc29a6897c95c222da06dab6c5683474d
Vladimir <Luxorchegone@gmail.com>
2021-06-03 19:33:58 +0300

    Написал разметку элемента списка-книг

--- src/components/book-list-item/book-list-item.js
+++ src/components/book-list-item/book-list-item.js
@@ -2,12 +2,27 @@ import React from 'react';
 import './book-list-item.css'
 
 const BookListItem = ({book}) => {
-    const {title, author} = book;
+    const {title, author, price, coverImage} = book;
     return (
-        <>
-            <span>{title}</span>
-            <span>{author}</span>
-        </>
+        <div className="book-list-item">
+            <div className="book-list-item__image-wrapper">
+                <img alt="book cover" src={coverImage} />
+            </div>
+            <div className="book-list-item__details">
+                <a href="#" className="book-list-item__title">
+                    {title}
+                </a>
+                <span className="book-list-item__author">
+                    {author}
+                </span>
+                <span className="book-list-item__price">
+                    ${price}
+                </span>
+                <button className="btn btn-info add-to-cart">
+                    Add to cart
+                </button>
+            </div>
+        </div>
     );
 }
 export default BookListItem;
