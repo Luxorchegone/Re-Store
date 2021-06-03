5d24e2ebbbc74b07412ec2afce1b181c0560af99
Vladimir <Luxorchegone@gmail.com>
2021-06-03 17:28:34 +0300

    Докинул фотографии и цены в сервис

--- src/services/book-store-service.js
+++ src/services/book-store-service.js
@@ -1,20 +1,29 @@
+// import ;
+
 export default class BookStoreService {
     getBooks() {
         return [
             {
                 id: 1,
                 title: 'Production-ready Microservices',
-                author: 'Suzan J. Fowler'
+                author: 'Suzan J. Fowler',
+                price: 11.99,
+                coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg'
+
             },
             {
                 id: 2,
                 title: 'Release It!',
-                author: 'Michael T. Nygard'
+                author: 'Michael T. Nygard',
+                price: 18.63,
+                coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41nMZPJdhsL._SX415_BO1,204,203,200_.jpg'
             },
             {
                 id: 3,
                 title: 'Grokking algorithms',
-                author: 'Adilya Y. Bhargava'
+                author: 'Adilya Y. Bhargava',
+                price: 34.32,
+                coverImage: 'https://images-na.ssl-images-amazon.com/images/I/51cV560hqBL._SX396_BO1,204,203,200_.jpg'
             }
         ];
     }
