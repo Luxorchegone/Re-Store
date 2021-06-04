dd4057b226af9514d36f89d3d513761d828c76e2
Vladimir <Luxorchegone@gmail.com>
2021-06-04 17:15:47 +0300

    Переименовал действия, создал новую функцию для получения списка книг(отделил ее от компонента book-list)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,30 @@
-const booksLoaded = (newBooks) => {
+const booksRequested = () => { //ставим флаг начала загрузки
     return {
-        type: 'BOOKS_LOADED',
+        type: 'FETCH_BOOKS_REQUEST',
+    }
+}
+
+const booksLoaded = (newBooks) => { //Записываем новую книгу
+    return {
+        type: 'FETCH_BOOKS_SUCCESS',
         payload: newBooks
     }
 }
 
-const booksRequested = () => {
+const booksError = (error) => { //Пишем ошибку
     return {
-        type: 'BOOKS_REQUESTED',
+        type: 'FETCH_BOOKS_FAILURE',
+        payload: error
     }
 }
+
+const fetchBooks = (dispatch, bookStoreService) => () => { 
+    dispatch(booksRequested()); //Ставим флаг зарузки
+    bookStoreService.getBooks() //получаем данные с "сервера"
+        .then((data) => dispatch(booksLoaded(data))) //Диспатчим данные в стору
+        .catch((error) => dispatch(booksError(error))); //Или диспатчим ошибку в стору
+}
+
 export {
-    booksLoaded,
-    booksRequested
+    fetchBooks
 }
