11d7b8ad0978be7702495280c1ed303a5533adf3
Vladimir <Luxorchegone@gmail.com>
2021-06-07 15:59:21 +0300

    Добавил разметку для таблицы в компонент заказа

--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import {connect} from 'react-redux'; 
+import {
+    bookAddedToCart,
+    bookRemovedFromCart,
+    allBooksRemovedFromCart } from '../../actions/';
 import './shopping-cart-table.css'
 
-const ShoppingCartTable = () => { //Таблица с заказом
+const ShoppingCartTable = ({items, orderTotal, onIncrease, onDecrease, onDelete}) => { //Таблица с заказом
+    const renderRow = (item, idx) => {
+        const {id, title, count, total} = item;
+        return (
+            <tr key={id}>
+                <td>{idx + 1}</td>
+                <td>{title}</td>
+                <td>{count}</td>
+                <td>${total}</td>
+                <td>
+                    <button className="shopping-cart__button btn btn-outline-danger btn-sm float-right"
+                            onClick={() => onDelete(id)}>
+                        <i className="fa fa-trash-o"/>
+                    </button>
+                    <button className="shopping-cart__button btn btn-outline-success btn-sm float-right"
+                            onClick={() => onIncrease(id)}>
+                        <i className="fa fa-plus-circle"/>
+                    </button> 
+                    <button className="shopping-cart__button btn btn-outline-warning btn-sm float-right"
+                            onClick={() => onDecrease(id)}>
+                        <i className="fa fa-minus-circle"/>
+                    </button>     
+                </td> 
+            </tr> 
+        );
+    }
+
     return (
         <div className="shopping-cart">
             <h2>Your order</h2>
@@ -16,29 +47,27 @@ const ShoppingCartTable = () => { //Таблица с заказом
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Site Reliability</td>
-                        <td>2</td>
-                        <td>$40</td>
-                        <td>
-                            <button className="shopping-cart__button btn btn-outline-danger btn-sm">
-                                <i className="fa fa-trash-o"/>
-                            </button>
-                            <button className="shopping-cart__button btn btn-outline-success btn-sm">
-                                <i className="fa fa-plus-circle"/>
-                            </button> 
-                            <button className="shopping-cart__button btn btn-outline-warning btn-sm">
-                                <i className="fa fa-minus-circle"/>
-                            </button>     
-                        </td> 
-                    </tr>  
+                    { items.map(renderRow) } 
                 </tbody>
             </table>
             <div className="shopping-cart__total-price">
-                Total: $201
+                ${orderTotal}
             </div>
         </div>
     )
 }
-export default ShoppingCartTable;
+
+const mapStateToProps = ({cartItems, orderTotal}) => {
+    return {
+        items: cartItems,
+        orderTotal: orderTotal,
+    }
+}
+
+const mapDispatchToProps = {
+    onIncrease: bookAddedToCart,
+    onDecrease: bookRemovedFromCart,
+    onDelete: allBooksRemovedFromCart
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCartTable);
