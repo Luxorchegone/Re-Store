7fe81ce590f6cd90daea1a90990f0f9c39a7be42
Vladimir <Luxorchegone@gmail.com>
2021-06-07 15:51:16 +0300

    Добавил действия для увеличения/уменьшения книг в заказе и действие для удаления позиции из заказа

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,93 @@
 const initialState = {
     books: [],
-    loading: true
+    loading: true,
+    error: null,
+    cartItems: [],
+    orderTotal: 115,
+}
+
+const updateCartItems = (cartItems, item, idx) => { //Формируем cartItems с новой позицией заказа
+    if (item.count == 0) {
+        return [
+            ...cartItems.slice(0, idx),
+            ...cartItems.slice(idx + 1)
+        ];
+    }
+
+    if (idx == -1) { //Если нет позиции в заказе, то добавляем ее
+        return [
+            ...cartItems,
+            item
+        ];
+    } else { //Если позиция уже есть но изменилось только количесвто, то обновляем ее
+        return [ 
+            ...cartItems.slice(0, idx),
+            item, 
+            ...cartItems.slice(idx + 1)
+        ]
+    }
+}
+
+const updateCartItem = (book, item = {}, quantity) => { //Формируем позицию заказа
+    const {
+        id = book.id, 
+        count = 0, 
+        title = book.title, 
+        total = 0 } = item;
+    
+    return {
+        id,
+        title,
+        count: count + quantity,
+        total: +(total + quantity*book.price).toFixed(2) //Привет IEE754
+    }
+}
+
+const updateOrder = (state, bookId, quantity) => {
+    const {books, cartItems} = state;
+    const book = books.find(({id}) => id == bookId);
+    const itemIndex = cartItems.findIndex(({id}) => id == bookId);
+    const item = cartItems[itemIndex];
+    
+    const newItem = updateCartItem(book, item, quantity);
+    return {
+        ...state,
+        cartItems: updateCartItems(cartItems, newItem, itemIndex)
+    }
 }
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "BOOKS_REQUESTED":
-            return {
-                books: [],
-                loading: true
-            }
-        case "BOOKS_LOADED":
-            return {
-                books: action.payload,
-                loading: false
+    console.log(action.type);
 
+    switch (action.type) { 
+        case 'FETCH_BOOKS_REQUEST': //Отправляем запрос на сервер 
+            return {
+                ...state,
+                books: [],
+                loading: true,
+                error: null
             }
+        case 'FETCH_BOOKS_SUCCESS': //Учпешно получаем ответ 
+            return {
+                ...state,
+                books: action.payload,
+                loading: false,
+                error: null
+            }
+        case 'FETCH_BOOKS_FAILURE': //Получаем ошибку 
+            return {
+                ...state,
+                books: [],
+                loading: false,
+                error: action.payload
+            }
+        case 'BOOK_ADDED_TO_CART': //Увеличиваем количество выбранной книги в заказе на 1
+            return updateOrder(state, action.payload, 1);
+        case 'BOOK_REMOVED_FROM_CART': //Уменьшаем количество выбранной книги в заказе на 1
+            return updateOrder(state, action.payload, -1);
+        case 'ALL_BOOKS_REMOVED_FROM_CART': //Удаляем ввсю позицию с книгой из заказа
+            const item = state.cartItems.find(({id}) => id == action.payload);
+            return updateOrder(state, action.payload, -item.count);
         default:
             return state;
     }
