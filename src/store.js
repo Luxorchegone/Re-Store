b75c3db1b4d85449320c83ec9ae6dc258c208622
Vladimir <Luxorchegone@gmail.com>
2021-06-08 12:48:29 +0300

    Пщупал redux-thunk и middleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,20 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducers';
+import thunkMiddleware from 'redux-thunk'; //просто попробовать
 
-const store = createStore(reducer);
+const logMiddleware = ({getState}) => (next) => (action) => { //Абсолютно бестолковый Middleware
+    console.log(action.type, getState());
+    return next(action);
+}
+
+const delayedActionCreator = (timeout) => (dispatch) => { //Абсолютно бестолковый экшен
+    setTimeout(() => dispatch({
+        type: 'DELAYED_ACTION'
+    }), timeout)
+}
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, logMiddleware));
+
+store.dispatch(delayedActionCreator(3000));
 
 export default store;
