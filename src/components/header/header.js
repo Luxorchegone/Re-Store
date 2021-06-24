dba3660b2dbc54e265a7bfd6276bfeb8913cffb9
Vladimir <Luxorchegone@gmail.com>
2021-06-24 11:47:43 +0300

    Подключил компонент к редаксу

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import './header.css';
 
-const Header = ({numItems, total}) => { //Шапка приложения
+const Header = ({orderTotal, itemCount}) => { //Шапка приложения
     return (
         <header className="page-header">
             <Link className="page-header__logo text-dark" to="/">
@@ -10,11 +11,17 @@ const Header = ({numItems, total}) => { //Шапка приложения
             </Link>
             <Link className="shopping-cart text-dark" to="/cart">
                 <i className="shopping-cart__icon fa fa-shopping-cart"/>
-                {numItems} items (${total})
+                {itemCount} items (${orderTotal})
             </Link>
 
         </header>
       
     );
 }
-export default Header;
+const mapStateToProps = ({shoppingCart: {orderTotal, itemCount}}) => {
+    return {
+        orderTotal,
+        itemCount
+    }
+}
+export default connect(mapStateToProps)(Header);
